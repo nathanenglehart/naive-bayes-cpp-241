bddaa8c7273be1c03cd35a31e1a94d79fdd04235
Nathan Englehart <88918730+nathanenglehart@users.noreply.github.com>
2021-12-08 17:53:42 -0500

    Create utils.h

---
+++ includes/utils.h
@@ -0,0 +1,19 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <fstream>
+#include "eigen3/Eigen/Dense"
+
+/* Nathan Englehart, Xuhang Cao, Samuel Topper, Ishaq Kothari (Autumn 2021) */
+
+double double_vector_list_lookup(std::vector<std::vector<double>>, int, int);
+double get_eigen_index(Eigen::VectorXd, int);
+bool compare_classification(const Eigen::VectorXd&, const Eigen::VectorXd&);
+Eigen::MatrixXd sorted_rows_by_classification(Eigen::MatrixXd);
+
+#endif
