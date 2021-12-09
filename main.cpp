be046f28e0b7a35cff27a2c6ce710cc564e65982
nathanenglehart <nengleha@oberlin.edu>
2021-12-08 22:47:44 -0500

    menu

--- main.cpp
+++ main.cpp
@@ -33,6 +33,17 @@ template<typename T> T load_csv(const std::string & sys_path)
   return Eigen::Map<const Eigen::Matrix<typename T::Scalar, T::RowsAtCompileTime, T::ColsAtCompileTime, Eigen::RowMajor>>(values.data(), rows, values.size()/rows);
 }
 
+bool valid_filepath(const std::string & sys_path)
+{
+  std::ifstream test(sys_path);
+  if(!test)
+  {
+    return false;
+  }
+
+  return true;
+}
+
 void driver(std::string sys_path_test, std::string sys_path_train, bool verbose)
 {
 
@@ -69,12 +80,84 @@ void driver(std::string sys_path_test, std::string sys_path_train, bool verbose)
   }
 }
 
-int main()
+int main(int argc, char ** argv)
 {
+
+    if(argc == 1)
+    {
+      std::cout << "Usage: ./naive-bayes-cli [train] [test] [options ..]\n";
+      std::cout << "More info with: \"./naive-bayes-cli -h\"\n";
+      return 1;
+    } else if(argc == 2) {
+    if(argv[1][0] == '-' && argv[1][1] == 'h' && argv[1][2] == '\0')
+    {
+      std::cout << "Naive Bayes Cli (2021 Dec 8, compiled " << __TIMESTAMP__ << " " << __TIME__ << ")\n\n";
+      std::cout << "usage: ./naive-bayes-cli [train] [test] [options ..]    read in train csv and test csv files from filesystem\n";
+      std::cout << "   or: ./naive-bayes-cli -h                             displays help menu\n\n";
+      std::cout << "Arguments:\n";
+      std::cout << "   -h     Displays help menu\n";
+      std::cout << "   -v     Displays output in verbose mode\n";
+      return 0;
+    } else
+    {
+      std::cout << "Unknown option argument: " << argv[1] << "\n";
+      std::cout << "More info with: \"./naive-bayes-cli -h\"\n";
+      return 1;
+    }
+  } else if(argc == 4)
+  {
+    if(argv[3][0] == '-' && argv[3][1] == 'v' && argv[3][2] == '\0') // Add check if file exists later
+    {
+      if(valid_filepath(argv[1]) && valid_filepath(argv[2]))
+      {
+        driver(argv[2],argv[1],true);
+        return 0;
+      } else if(!valid_filepath(argv[1]))
+      {
+        std::cout << "Invalid filepath: " << argv[1] << "\n";
+        return 1;
+      } else if(!valid_filepath(argv[2]))
+      {
+        std::cout << "Invalid filepath: " << argv[2] << "\n";
+        return 1;
+      }
+    } else
+    {
+      std::cout << "Unknown option argument: " << argv[3] << "\n";
+      std::cout << "More info with: \"./naive-bayes-cli -h\"\n";
+      return 1;
+    }
+  } else if(argc == 3) // Add check if file exists later
+  {
+    if(valid_filepath(argv[1]) && valid_filepath(argv[2]))
+    {
+      driver(argv[2],argv[1],true);
+      return 0;
+    } else if(!valid_filepath(argv[1]))
+    {
+      std::cout << "Invalid filepath: " << argv[1] << "\n";
+      return 1;
+    } else if(!valid_filepath(argv[2]))
+    {
+      std::cout << "Invalid filepath: " << argv[2] << "\n";
+      return 1;
+    } else
+    {
+      std::cout << "Unknown option argument: " << argv[3] << "\n";
+      std::cout << "More info with: \"./naive-bayes-cli -h\"\n";
+      return 1;
+    }
+  } else
+  {
+    std::cout << "Usage: ./naive-bayes-cli [train] [test] [options ..]\n";
+    std::cout << "More info with: \"./naive-bayes-cli -h\"\n";
+    return 1;
+
+  }
 
   /* [Iris-virginica] => 0 [Iris-setosa] => 1 [Iris-versicolor] => 2 */
 
-  driver("./data/iristest.csv","./data/iris.csv",true);
+  //driver("./data/iristest.csv","./data/iris.csv",true);
 
   //driver("./data/S1test.csv","./data/S1train.csv",true);
   //driver("./data/S2test.csv","./data/S2train.csv",true);
