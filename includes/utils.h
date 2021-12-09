ed85db0bbf36f107c4b92c4200dcbb1511be3e3c
Nathan Englehart <88918730+nathanenglehart@users.noreply.github.com>
2021-12-09 02:27:02 -0500

    Update utils.h

--- includes/utils.h
+++ includes/utils.h
@@ -11,6 +11,8 @@
 
 /* Nathan Englehart, Xuhang Cao, Samuel Topper, Ishaq Kothari (Autumn 2021) */
 
+float round(float);
+bool valid_filepath(const std::string &);
 double double_vector_list_lookup(std::vector<std::vector<double>>, int, int);
 double get_eigen_index(Eigen::VectorXd, int);
 bool compare_classification(const Eigen::VectorXd&, const Eigen::VectorXd&);
